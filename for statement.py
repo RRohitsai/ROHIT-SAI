c0f53e105aabd1241c50e0ee9905b9f71d93bafc
RRohitsai <167394644+RRohitsai@users.noreply.github.com>
2024-04-26 12:33:23 +0530

    Create for statement.py

---
+++ for statement.py
@@ -0,0 +1,16 @@
+'''
+item is a variable that takes on each value in the iterable object iterable one by one, 
+and the code block under the loop is executed for each value of item.
+'''
+
+Syntax:
+
+for item in iterable:
+    # code block to be executed for each item
+
+Program:
+
+my_list = [1, 2, 3, 4, 5]
+
+for number in my_list:
+    print(number)
