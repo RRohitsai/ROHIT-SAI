5b1d8caa8175149e2abb3ab3c6bc79a165806024
RRohitsai <167394644+RRohitsai@users.noreply.github.com>
2024-10-21 11:28:08 +0530

    Create Input's.py

---
+++ Java/Input's.py
@@ -0,0 +1,9 @@
+While giving any input from the user we should use 
+import Java.util.Scanner; and Scanner sc= new Scanner( System.in);
+Using this function we can take any input from the user 
+
+Their are some keywords are 
+For String we use next();
+For int we use nextInt();
+For any primitive datatype we use nextdatatype 
+hasnext() is use to verify that the elements are store are not
